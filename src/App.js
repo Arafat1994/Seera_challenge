6606c5f9abc98c9165401a622e0e4ab732669f60
arafat <arafat1994@outlook.sa>
2022-07-16 20:53:52 +0300

    editing the ruter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
     
       <Routes>
-        <Route path='/' element={<HomeComp/>} /> 
+        <Route path='' element={<HomeComp/>} /> 
         <Route path='/HotelListing' element={<HotelListing/>} /> 
       </Routes>
 
