45c0de9e56227ccb9536b30c8fb65800e1d93ac4
arafat <arafat1994@outlook.sa>
2022-07-16 22:32:04 +0300

    add router @6

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.scss';
 import HomeComp from './Components/Home_Component';
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import HotelListing from './Components/HotelListing';
-import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route exact path='/Seera_challenge' element={<HomeComp />} />
-          <Route path='/HotelListing' element={<HotelListing />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path='/Seera_challenge' element={<HomeComp />} />
+            <Route path='/HotelListing' element={<HotelListing />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 export default App;
