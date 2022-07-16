6c9ea34d7f2a51e76bc1764770a807ef7eed396e
arafat <arafat1994@outlook.sa>
2022-07-16 22:27:53 +0300

    edit router config

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.scss';
 import HomeComp from './Components/Home_Component';
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import HotelListing from './Components/HotelListing';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    
-      <Routes>
-        <Route path='' element={<HomeComp/>} /> 
-        <Route path='/HotelListing' element={<HotelListing/>} /> 
-      </Routes>
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path='/Seera_challenge' element={<HomeComp />} />
+          <Route path='/HotelListing' element={<HotelListing />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
